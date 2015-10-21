a8819bf9c727a9e00b5f68bb287d01ff07a49c5b
sarahsalder <sarahsalder@gmail.com>
2015-10-20 20:44:20 -0400

    adds interface for creating communities

--- public/site/angular/controllers/community.js
+++ public/site/angular/controllers/community.js
@@ -2,6 +2,14 @@ var communityCtr = angular.module('CommunityModule', []);
 
 communityCtr.controller('CommunityController', ['$scope', 'accountService', 'generalService', 'uploadService', 'RestService', function($scope, accountService, generalService, uploadService, RestService) {
 
+  $scope.community = { 
+    'name':'',
+    'website': '',
+    'description': '',
+    'url': '',
+    'image': '',
+    'password': ''
+  }
   
   $scope.init = function(){
     console.log('CommunityController: INIT');
@@ -9,7 +17,9 @@ communityCtr.controller('CommunityController', ['$scope', 'accountService', 'gen
     accountService.checkCurrentUser(function(response){
       if (response.confirmation == 'success')
         $scope.profile = response.profile;
+
       fetchCommunities();
+
     });
   }
 
@@ -19,6 +29,41 @@ communityCtr.controller('CommunityController', ['$scope', 'accountService', 'gen
         return;
       
       console.log('Communities == '+JSON.stringify(response));
+    });
+  }
+
+  $scope.createCommunity = function (){
+    console.log('create community called:' +JSON.stringify($scope.community));
+    RestService.post({resource:'community'}, $scope.community, function(response){
+      if (response.confirmation != 'success')
+        alert('post request failed');
+
+      
+
+    });
+
+  }
+
+  $scope.onFileSelect = function(files, property, media){
+
+    
+    uploadService.uploadFiles({'files':files, 'media':media}, function(response, error){
+      
+      if (error != null){
+        alert(error.message);
+        return;
+      }
+      
+      if (media != 'images')
+        return;
+
+      var image = response.image;
+      console.log(JSON.stringify(image));
+      
+      $scope.community.image = image.id;
+ 
+      console.log($scope.community);
+
     });
   }
   
