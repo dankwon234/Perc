1d8151df3cae1c719928655c4b1d2a7632f4d384
dankwon234 <dan.kwon234@gmail.com>
2015-10-21 20:08:31 -0400

    added title property to Profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var ProfileSchema = new mongoose.Schema({
 	firstName: {type:String, trim:true, lowercase:true, default:''},
 	lastName: {type:String, trim:true, lowercase:true, default:''},
+	title: {type:String, trim:true, default:''},
 	url: {type:String, trim:true, default:''}, // url path, e.g. www.getpercs.com/profile/dan-kwon234
 	email: {type:String, trim:true, lowercase:true, default:''},
 	bio: {type:String, trim:true, default:''},
@@ -18,6 +19,7 @@ ProfileSchema.methods.summary = function() {
 	var summary = {
 		'firstName':this.firstName,
 		'lastName':this.lastName,
+		'title':this.title,
 		'url':this.url,
 		'email':this.email,
 		'bio':this.bio,
