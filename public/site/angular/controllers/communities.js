f2a5748ce3ca5c1fb9aef082e2f1d74688ebca24
dankwon234 <dan.kwon234@gmail.com>
2015-10-23 19:26:24 -0400

    fetch communities on init

--- public/site/angular/controllers/communities.js
+++ public/site/angular/controllers/communities.js
@@ -5,13 +5,38 @@ communitiesCtr.controller('CommunitiesController', ['$scope', 'accountService', 
 
 
     $scope.init = function(){
-        console.log('CommunitiesController: INIT!');
-        
-
-
+        RestService.query({resource:'community', id:null}, function(response){
+            if (response.confirmation != 'success')
+                return;
+            
+            $scope.communities = response.communities;
+            console.log('CommunitiesController: '+JSON.stringify($scope.communities));
+        });
     }
 
-  
+    $scope.joinCommunity = function(community){
+    	if ($scope.profile.id == null)
+    		return;
+
+    	if ($scope.profile.communities.indexOf(community.id) != -1){ // already a member
+    		window.location.href = '/';
+    		return;
+    	}
+
+    	$scope.profile.communities.unshift(community.id); // add at top of array
+    	accountService.updateProfile($scope.profile, function(){
+	    	community['numMembers'] = community.numMembers+1;
+	        RestService.put({resource:'community', id:community.id}, community, function(response){
+	            if (response.confirmation != 'success')
+	                return;
+	            
+	            console.log('CommunitiesController: '+JSON.stringify(response));
+	            window.location.href = '/';
+	        });
+
+    	});
+    }
+
 
   
   
