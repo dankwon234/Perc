68120a28e6e70b6eda95ebd693a19148b48890ca
dankwon234 <dan.kwon234@gmail.com>
2015-10-23 14:49:19 -0400

    new controller for communities page

--- public/site/angular/app.js
+++ public/site/angular/app.js
@@ -3,6 +3,7 @@ var modules = [
   'BaseModule',
 	'HomeModule',
   'CommunityModule',
+  'CommunitiesModule',
 	'GeneralServiceModule',
 	'RestServiceModule',
 	'UploadServiceModule',
---
+++ public/site/angular/controllers/communities.js
@@ -0,0 +1,22 @@
+var communitiesCtr = angular.module('CommunitiesModule', []);
+
+communitiesCtr.controller('CommunitiesController', ['$scope', 'accountService', 'generalService', 'uploadService', 'RestService', function($scope, accountService, generalService, uploadService, RestService) {
+    $scope.communities = null;
+
+
+    $scope.init = function(){
+        console.log('CommunitiesController: INIT!');
+        
+
+
+    }
+
+  
+
+  
+  
+  
+  
+  
+
+}]);
