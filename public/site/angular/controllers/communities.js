ac5fea042b9983c481b114fc01aa6ee81f52b74e
dankwon234 <dan.kwon234@gmail.com>
2015-10-26 23:27:23 -0400

    added more placeholders for communities array

--- public/site/angular/controllers/communities.js
+++ public/site/angular/controllers/communities.js
@@ -1,7 +1,7 @@
 var communitiesCtr = angular.module('CommunitiesModule', []);
 
 communitiesCtr.controller('CommunitiesController', ['$scope', 'accountService', 'generalService', 'uploadService', 'RestService', function($scope, accountService, generalService, uploadService, RestService) {
-    $scope.communities = ['','']; // need placeholders here to activate the js that animates the post panels
+    $scope.communities = ['','','','','','','','','','','','','','','','','','']; // need placeholders here to activate the js that animates the post panels
 
 
     $scope.init = function(){
