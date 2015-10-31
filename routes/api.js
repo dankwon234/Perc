01344aed4af314afa274366a533791f23c0c5b61
dankwon234 <dan.kwon234@gmail.com>
2015-10-30 20:50:42 -0400

    added final email template

--- routes/api.js
+++ routes/api.js
@@ -149,7 +149,7 @@ router.post('/:resource', function(req, res, next) {
 		
 		recipients.push('dennykwon2@gmail.com');
 		
-		fetchFile('public/email/intro/email.html')
+		fetchFile('public/email/intro/final.html')
 		.then(function(data){
 			var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 			for (var i=0; i<recipients.length; i++){
