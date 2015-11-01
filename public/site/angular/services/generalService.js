d455a83691a26747597ee636e69280ad81687c5b
dankwon234 <dan.kwon234@gmail.com>
2015-10-31 21:32:49 -0400

    added capitalize to general service

--- public/site/angular/services/generalService.js
+++ public/site/angular/services/generalService.js
@@ -10,6 +10,29 @@ generalService.factory('generalService', [function(){
 		
 		return text.substring(0, limit)+'...';
 	}
+    
+    manager.capitalize = function(string) {
+        if (string == null)
+            return;
+        
+        var parts = string.split(' ');
+        var capitalizedString = '';
+        
+        for (var i=0; i<parts.length; i++){
+            var s = parts[i];
+            if (s.length <= 1){
+                capitalizedString = capitalizedString+' '+s.toUpperCase();
+                continue;
+            }
+            
+            s = s.charAt(0).toUpperCase() + s.slice(1);
+            capitalizedString = capitalizedString+' '+s;
+        }
+        
+        capitalizedString = capitalizedString.trim();
+        return capitalizedString;
+    }
+
 	
 	
     manager.formattedDate = function(dateStr) {
