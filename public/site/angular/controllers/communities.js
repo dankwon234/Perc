ffeb3833c17e758203f49a1699890f9db93bdc70
dankwon234 <dan.kwon234@gmail.com>
2015-11-07 01:44:51 -0500

    select community

--- public/site/angular/controllers/communities.js
+++ public/site/angular/controllers/communities.js
@@ -2,6 +2,7 @@ var communitiesCtr = angular.module('CommunitiesModule', []);
 
 communitiesCtr.controller('CommunitiesController', ['$scope', 'accountService', 'generalService', 'uploadService', 'RestService', function($scope, accountService, generalService, uploadService, RestService) {
     $scope.communities = ['','','','','','','','','','','','','','','','','','']; // need placeholders here to activate the js that animates the post panels
+    $scope.selectedCommunity = null;
 
 
     $scope.init = function(){
@@ -10,8 +11,13 @@ communitiesCtr.controller('CommunitiesController', ['$scope', 'accountService', 
                 return;
             
             $scope.communities = response.communities;
-            console.log('CommunitiesController: '+JSON.stringify($scope.communities));
+            $scope.selectedCommunity = $scope.communities[0];
         });
+    }
+
+    $scope.selectCommunity = function(community){
+        $scope.selectedCommunity = community;
+
     }
 
     $scope.joinCommunity = function(community){
