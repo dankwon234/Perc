83c6be997112e941da0910a98038faaf285139fd
dankwon234 <dan.kwon234@gmail.com>
2015-11-07 00:26:22 -0500

    fetch profile community on account page

--- public/site/angular/controllers/account.js
+++ public/site/angular/controllers/account.js
@@ -5,7 +5,25 @@ homeCtr.controller('AccountController', ['$scope', 'accountService', 'generalSer
 
 	$scope.init = function(){
 		console.log('AccountController: INIT');
+		if ($scope.profile.id != null){
+			fetchProfileCommunity();
+			return;
+		}
 
+		// BaseController checks for current user. If found, this listener gets trieggered:
+		$scope.$watch('profile', function(newValue, oldValue){
+			fetchProfileCommunity();
+		}, true);
+	}
+
+	function fetchProfileCommunity(){
+		RestService.query({resource:'community', id:$scope.profile.communities[0]}, function(response){
+			console.log('AccountController: fetchProfileCommunity - '+JSON.stringify(response));
+			if (response.confirmation != 'success')
+				return;
+
+			$scope.currentCommunity = response.community;
+		});
 	}
 	
 
